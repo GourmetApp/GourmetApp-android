fc7ff9e203d5aa19a38e0abac4528091488a9e05
Javier González <javierugarte91@gmail.com>
2017-07-22 00:10:53 +0200

    New notification implementation

--- app/src/main/java/com/jugarte/gourmet/Application.java
+++ app/src/main/java/com/jugarte/gourmet/Application.java
@@ -1,17 +1,11 @@
 package com.jugarte.gourmet;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.os.SystemClock;
-
 import com.crashlytics.android.Crashlytics;
 import com.google.firebase.database.FirebaseDatabase;
 import com.jugarte.gourmet.di.component.ApplicationComponent;
 import com.jugarte.gourmet.di.component.DaggerApplicationComponent;
 import com.jugarte.gourmet.di.module.ApplicationModule;
-import com.jugarte.gourmet.services.GourmetReceiver;
+import com.jugarte.gourmet.services.NotificationUtils;
 import com.jugarte.gourmet.tracker.Tracker;
 
 import io.fabric.sdk.android.Fabric;
@@ -32,14 +26,7 @@ public class Application extends android.app.Application {
                 .applicationModule(new ApplicationModule(this)).build();
         applicationComponent.inject(this);
 
-        Intent alarm = new Intent(getApplicationContext(), GourmetReceiver.class);
-        boolean alarmRunning = (PendingIntent.getBroadcast(getApplicationContext(), 0, alarm, PendingIntent.FLAG_NO_CREATE) != null);
-        if (!alarmRunning) {
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, alarm, 0);
-            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), 4 * 60 * 1000, pendingIntent);
-        }
-
+        NotificationUtils.startNotificationService(this);
     }
 
     public ApplicationComponent getComponent() {
---
+++ app/src/main/java/com/jugarte/gourmet/services/NotificationService.java
@@ -0,0 +1,115 @@
+package com.jugarte.gourmet.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.jugarte.gourmet.BuildConfig;
+import com.jugarte.gourmet.R;
+import com.jugarte.gourmet.ThreadManager;
+import com.jugarte.gourmet.ThreadManagerImp;
+import com.jugarte.gourmet.data.prefs.AppPreferencesHelper;
+import com.jugarte.gourmet.domine.beans.Gourmet;
+import com.jugarte.gourmet.domine.gourmet.GetGourmet;
+import com.jugarte.gourmet.domine.user.GetUser;
+import com.jugarte.gourmet.ui.balance.BalanceActivity;
+
+public class NotificationService extends Service implements GetGourmet.OnGourmetResponse {
+
+    public static final int SERVICE_REQUEST_CODE = 1;
+    public static final int NOTIFICATION_ID = 1;
+
+    private ThreadManager tm;
+    GetGourmet getGourmet;
+    GetUser getUser;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getGourmet = new GetGourmet();
+        getUser = new GetUser(new AppPreferencesHelper(getApplicationContext()));
+        tm = new ThreadManagerImp();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        if (getUser.isLogged()) {
+            checkDataChanges();
+        }
+        stopSelf(startId);
+        return Service.START_NOT_STICKY;
+    }
+
+    private void checkDataChanges() {
+
+        final String user = getUser.getUser();
+        final String password = getUser.getPassword();
+        tm.runOnBackground(new Runnable() {
+            @Override
+            public void run() {
+                getGourmet.execute(user, password, NotificationService.this);
+            }
+        });
+    }
+
+    @Override
+    public void success(final Gourmet gourmet) {
+        tm.runOnUIThread(new Runnable() {
+            @Override
+            public void run() {
+                showNotification(gourmet);
+            }
+        });
+    }
+
+    @Override
+    public void notConnection(Gourmet cacheGourmet) {
+
+    }
+
+    @Override
+    public void notUserFound() {
+
+    }
+
+    private void showNotification(Gourmet gourmet) {
+        if (!BuildConfig.DEBUG && gourmet.getNewOperations() == 0) {
+            return;
+        }
+
+        String text = String.format(getResources().getQuantityString(R.plurals.new_operations,
+                gourmet.getNewOperations()),
+                gourmet.getNewOperations());
+
+        Intent intent = new Intent(this, BalanceActivity.class);
+        intent.putExtra(BalanceActivity.EXTRA_GOURMET, gourmet);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setTicker("Nuevas operaciones")
+                        .setContentTitle("Nuevas operaciones")
+                        .setContentText(text)
+                        .setColor(ContextCompat.getColor(getApplicationContext(), R.color.accent))
+                        .setAutoCancel(true)
+                        .setContentIntent(pendingIntent);
+
+        ((NotificationManager) getSystemService(NOTIFICATION_SERVICE))
+                .notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+}
